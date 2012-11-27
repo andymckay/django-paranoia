9d773dd0426902f45a64e3552317e552240bac6a
Andy McKay <amckay@mozilla.com>
2012-11-27 11:01:10 -0800

    get package ready

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+
+setup(
+    name='django-paranoia',
+    version='0.1',
+    description='OWASP detection point reporting for Django',
+    long_description=open('README.rst').read(),
+    author='Andy McKay',
+    author_email='andym@mozilla.com',
+    license='BSD',
+    install_requires=['Django'],
+    packages=['django_paranoia',
+              'django_paranoia/reporters'],
+    url='https://github.com/andymckay/django-paranoia',
+    include_package_data=True,
+    zip_safe=False,
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Framework :: Django'
+    ]
+)
