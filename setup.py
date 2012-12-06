c6e273dc499b1153712ae0cdedf57e2fc25dd32a
Andy McKay <amckay@mozilla.com>
2012-12-05 18:18:05 -0800

    0.1.1 release

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='django-paranoia',
-    version='0.1',
+    version='0.1.1',
     description='OWASP detection point reporting for Django',
     long_description=open('README.rst').read(),
     author='Andy McKay',
