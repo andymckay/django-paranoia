e09d84738b39e7912530253bc620922a88ed652b
Andy McKay <amckay@mozilla.com>
2012-12-07 14:28:48 -0800

    add in decorators RE1 to RE4

--- django_paranoia/flags.py
+++ django_paranoia/flags.py
@@ -1,17 +1,20 @@
 from django.utils.translation import ugettext as _
 
-EXTRA_FIELDS = 'extra-form-fields'
-MISSING_FIELDS = 'missing-form-fields'
+EXTRA_FIELDS = 'extra-form-fields'  # OWASP: RE6
+MISSING_FIELDS = 'missing-form-fields'  # OWASP: RE5
+WRONG_METHOD = 'wrong-method'  # OWASP: RE1, RE2, RE3 and RE4
 SQL_INJECTION = 'sql-injection-attempt'
-XSS = 'xss-attempt'
 SESSION_CHANGED = 'session-changed'
+XSS = 'xss-attempt'
 
 trans = {
     EXTRA_FIELDS: _('Attempt to process form with extra values'),
     MISSING_FIELDS: _('Attempt to process form with missing values'),
     SQL_INJECTION: _('Data looks that like SQL injection attempt'),
-    XSS: _('Data that looks like XSS attempt'),
     SESSION_CHANGED: _('Session data changed'),
+    WRONG_METHOD: _('Wrong HTTP method'),
+    XSS: _('Data that looks like XSS attempt'),
 }
 
-__all__ = [EXTRA_FIELDS, MISSING_FIELDS, SQL_INJECTION, XSS, SESSION_CHANGED]
+__all__ = [EXTRA_FIELDS, MISSING_FIELDS, WRONG_METHOD, SQL_INJECTION,
+           SESSION_CHANGED, XSS]
--- django_paranoia/signals.py
+++ django_paranoia/signals.py
@@ -1,5 +1,8 @@
 from django.dispatch import Signal
 
+# These are used internally to hook up to the request finished signal.
 finished = Signal(providing_args=['request'])
 process = Signal(providing_args=['type', 'message', 'values', 'request'])
+
+# This is the signal that you hook the warnings in the code up to.
 warning = Signal(providing_args=['type', 'message', 'values'])
